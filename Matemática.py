ae318a5bf39e58d2733cc72b43b1201ea33fe067
Jhonatas de Oliveira <110613363+devjhonatas@users.noreply.github.com>
2023-03-23 16:54:25 -0400

    Add files via upload
    Trabalhos da faculdade

---
+++ Matemática.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Solicita ao usuário que insira  um número
+
+
+# In[9]:
+
+
+num = int(input("Insira um número"))
+
+# Exibe a tabuada desse número
+
+print("Tabuada de", num)
+for i in range(1, 11):
+    print(num, "x", i, "=", num*i)
+    
+
+print('BONS ESTUDOS :)')
+
+
+# In[ ]:
+
+
+
+
